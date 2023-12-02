7da7b5c38c955e3fa534dea5af50fc77da2ce90a
Ryan Kenney <rkenney@moesol.com>
2023-12-02 07:56:27 -0800

    Combined build and run scripts

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+set -e -o pipefail -u
+
+sudo docker build -t advent-of-code-2023:local-build .
+
 sudo docker run --rm -p 127.0.0.1:8080:8080 -it advent-of-code-2023:local-build
 
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t advent-of-code-2023:local-build .
-
