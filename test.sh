8d9c0690904aedbcc753a8c15e52a699fc5de94a
Ryan Kenney <rkenney@moesol.com>
2023-12-04 17:46:28 -0800

    Updated tesh.sh (and build-test.sh) to build all nested unit tests

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 # Fail on any error or undefined variable
 set -e -o pipefail -u
 
-sudo docker run --rm -it --name advent-of-code-2023-test advent-of-code-2023-builder:local-build go test
+sudo docker run --rm -it --name advent-of-code-2023-test advent-of-code-2023-builder:local-build go test ./...
